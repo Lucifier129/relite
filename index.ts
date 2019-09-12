b91b27af1e4b85c315b673a37f07dd438f947204
Monsooon <mtq1997@126.com>
2019-09-12 17:12:05 +0800

    add some comment&&fix package.json&&update gitignore

--- index.ts
+++ index.ts
@@ -576,4 +576,13 @@ export const createStore: StoreCreator = <
   return store
 }
 
+/**
+ * Get the Object.keys() as keyof inputed object.
+ * 
+ * @template T The typeof inputed object. 
+ * 
+ * @param o A value extends object.
+ * 
+ * @returns Object.keys(o) with type keyof T.
+ */
 const getKeys = <T extends {}>(o: T) => Object.keys(o) as Array<keyof T>
